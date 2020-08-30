6daa32dd55b2393006285731630e90b68955cdb8
lromanova <perfectzvekla@gmail.com>
2020-08-30 20:26:22 +0300

    Фамилия

---
+++ app/src/main/java/com/sitaara/appbarver1/ConstraintCalc.java
@@ -0,0 +1,14 @@
+package com.sitaara.appbarver1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintCalc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_calc);
+    }
+}
